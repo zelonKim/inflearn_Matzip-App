993dffb6c1173abc3207cd324eb7f6a37cac6fb7
zelonKim <ksz18601@gmail.com>
2025-07-04 13:38:18 +0900

    GeoLocation

---
+++ front/src/hooks/useUserLocation.ts
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react';
+import {LatLng} from 'react-native-maps';
+import GeoLocation from '@react-native-community/geolocation';
+
+function useUserLocation() {
+  const [userLocation, setUserLocation] = useState<LatLng>({
+    latitude: 37.5516032365118,
+    longitude: 126.98989626020192,
+  });
+  
+  const [isUserLocationError, setIsUserLocationError] = useState(false);
+
+
+  useEffect(() => {
+    GeoLocation.getCurrentPosition(
+      info => {
+        // 첫번째 인수 -> 성공 시 실행될 콜백함수 (매개변수에 위치정보 객체가 담김.)
+        const {latitude, longitude} = info.coords; // 위치정보 객체.coords: 현재 위치의 좌표값을 가져옴.
+        setUserLocation({latitude, longitude});
+        setIsUserLocationError(false);
+      },
+      () => {
+        // 두번째 인수 -> 에러 발생 시, 실행될 콜백함수
+        setIsUserLocationError(true);
+      },
+      {
+        enableHighAccuracy: true, // 세번째 인수 -> 옵션
+      },
+    );
+  }, []);
+
+  return {userLocation, isUserLocationError};
+}
+
+export default useUserLocation;
--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {  useRef } from 'react';
 import {StyleSheet, View, Text, Button, Pressable} from 'react-native';
-import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
-import useAuth from '../../hooks/queries/useAuth';
+import {  useSafeAreaInsets } from 'react-native-safe-area-context';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { colors } from '@/constants';
 import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
@@ -9,7 +8,7 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { MapStackParamList } from '@/navigations/stack/MapStackNavigator';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
 import { MainDrawerParamList } from '@/navigations/drawer/MainDrawerNavigator';
-
+import useUserLocation from '@/hooks/useUserLocation';
 
 
 type Navigation = CompositeNavigationProp<
@@ -22,20 +21,44 @@ const MapHomeScreen() {
   const inset = useSafeAreaInsets();
   const navigation = useNavigation<Navigation>();
 
+  const mapRef = useRef<MapView | null>(null);
+
+  const {userLocation, isUserLocationError} = useUserLocation()
+
+
+const handlePressUserLocation = () => {
+  if(isUserLocationError) {
+    return
+  }
+  mapRef.current?.animateToRegion({ // 지도를 해당 좌표로 이동시킴.
+    latitude: userLocation.latitude,
+    longitude: userLocation.longitude,
+    longitudeDelta: 0.0421, // 확대 정도
+    latitudeDelta: 0.0922
+  })
+}
 
   return (
     <>
        <MapView 
+        ref={mapRef}
         style={styles.container} 
         provider={PROVIDER_GOOGLE} 
         showsUserLocation
         followsUserLocation
         showsMyLocationButton={false}
         />
+
         <Pressable style={[styles.drawerButton, {top: inset.top || 20}]} 
             onPress={ () => navigation.openDrawer()}>
           <Text>서랍</Text>
         </Pressable>
+        
+        <View style={styles.buttonList}>
+          <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+            <Text>내 위치</Text>
+          </Pressable>
+        </View>
     </>
   )
 }
@@ -58,6 +81,24 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 1, height: 1},
     shadowOpacity: 0.5,
     elevation: 4
+  },
+  buttonList: {
+    position: 'absolute',
+    bottom: 30,
+    right: 15
+  },
+  mapButton: {
+    backgroundColor: colors.PINK_700,
+    marginVertical: 5,
+    height: 48,
+    width: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.5,
+    elevation: 2,
   }
 });
 
